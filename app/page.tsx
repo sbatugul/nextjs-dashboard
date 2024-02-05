a9f6c96d5fb9c1516f11e9e7ab2b0deb88b7c42f
sbatugul <sbatuhuangul@hotmail.com>
2024-02-05 20:33:15 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+
 <div className={styles.shape} />;
 
 export default function Page() {
